0d7547c3f90c6212e4dfef19efd3a0ff2c75f79a
Benjamin Jenkins <bjenkins22@live.com>
2017-07-10 16:03:29 -0400

    first ui commit

---
+++ AdminLTE-2.3.11/dist/js/list_view.js
@@ -0,0 +1,12 @@
+$(document).ready( function () {
+  var dataset = [["File One","Complete","01/02/03","None"],["File Two","Complete","02/03/04","None"]]
+  $('#list-view-table').DataTable({
+    data: dataset,
+    columns: [
+      {title: "File Name"},
+      {title: "Transfer Status"},
+      {title: "Date"},
+      {title: "Action Items"}
+    ]
+  });
+} );
