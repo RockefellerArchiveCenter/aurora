7bf5e1089aa057fbd14f473aec6e0740297c9033
HaSistrunk <hsistrunk@rockarch.org>
2017-08-14 15:21:34 -0400

    add file size column to transfer status UI

--- AdminLTE-2.3.11/dist/js/transfer_log.js
+++ AdminLTE-2.3.11/dist/js/transfer_log.js
@@ -1,5 +1,5 @@
 $(document).ready( function () {
-  var dataset = [["Transfer File One","Complete","Date-Time","None"],["Transfer File Two","Complete","Date-Time","None"]]
+  var dataset = [["Transfer File One","Complete","Date-Time", "File Size", "None"],["Transfer File Two","Complete","Date-Time", "File Size", "None"]]
 
   $('#transfer-log-table').DataTable({
     data: dataset,
@@ -7,6 +7,7 @@ $(document).ready( function () {
       {title: "Transfer File Name"},
       {title: "Transfer Status"},
       {title: "Date/Time"},
+      {title: "Size"},
       {title: "Action Items"}
     ]
   });
