9c17e555c632a9ff5a2bfce5a6789ea8099d0bc3
John A Digilio <John.Digilio@marist.edu>
2017-08-31 13:50:21 -0400

    Modified upload.sh to use variables to define paths and filenames.

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,10 +1,47 @@
 #!/bin/bash
 #
-# Script to identify new file uploads - run every 30 minutes - run log is in /data/logs/uploads
+# Script to identify new file uploads - run every 30 minutes
 #
-
-# Get the date and time now and from 30 minutes ago. Between 00:00 and 00:29 the date will be the previous day.
-
+#
+# Set the following variables for your environment - you may also need to adjust the name and format 
+# of the rotated messages file if the format differs from the default used below.
+#
+path_to_messages_log="/var/log/"
+messages_file_name="messages"
+path_to_rotated_messages_log="/var/log/"
+path_to_temp_extract_file="/data/"
+temp_extract_file_name="new_uploads"
+path_to_clamav_log_file="/data/logs/clamav/"
+clamav_update_log_file_name="/clamupdate.log"
+path_to_output_file="/data/"
+output_file_name="uploads_to_process"
+#
+# End of Variable setup
+#
+# Build filenames and paths
+#
+messages_file=$path_to_messages_log
+messages_file+=$messages_file_name
+clamav_log=$path_to_clamav_log_file
+clamav_log+=$clam_update_log_file_name
+temp_file=$path_to_temp_extract_file
+temp_file+=$temp_extract_file_name
+output_file=$path_to_output_file
+output_file+=$output_file_name
+#
+# Build the rotated file name to check to see if it exists (local format is /var/log/messages-YYMMDD.xz - adjust as needed).
+#
+rfile=$path_to_rotated_messages_log
+rfile+="messages-"
+if [ !$date_now = $date_30mins_ago ]; then
+   rfile+=$date_30mins_ago2
+else
+   rfile+=$date_now2
+fi
+rfile+=".xz"
+#
+# Get the date and time now and from 30 minutes ago. 
+#
 date_now="$(date +'%Y-%m-%d')"
 date_now2="$(date +'%Y%m%d')"
 time_now="$(date +'%T')"
@@ -14,41 +51,32 @@ time_30mins_ago="$(date -d '-30 mins' +'%T')"
 date_time_now="$date_now $time_now"
 date_time_30mins_ago="$date_30mins_ago $time_30mins_ago"
 echo "Run: " $date_time_now
-
-# Build the rotated file name to check to see if it exists
-
-rfile="/var/log/messages-"
-if [ !$date_now = $date_30mins_ago ]; then
-   rfile+=$date_30mins_ago2
-else
-   rfile+=$date_now2
-fi
-rfile+=".xz"
-
-# Read in the rotated log file (if one exists) and /var/log/messages then split the single data-time
+#
+# Read in the rotated log file if one exists ($rfile) and $messages_file then split the single data-time
 # field in the records into two separate fields.  There is a "T" between the date and the time.  
 # Keep only the records contain both "sftp-server" and  "close" from the last 30 minutes.  
-# Write the records to /data/new_uploads.  Add an "EOF" record to the file in case there 
+# Write the records to $temp_file.  Add an "EOF" record to the file in case there 
 # are no uploads to process in this run.
-
+#
+echo "***** START ***** Run:" $date_time_now "*****" > $temp_file
 if [ -e $rfile ]; then
    unxz -c -d $rfile | awk -FT '{print $1 " "  $2}' | grep "sftp\-server.*close " |
-   awk -v "b=$date_time_30mins_ago" -v "e=$date_time_now" -F '.' '$1 >= b && $1 <= e' > /data/new_uploads
-   awk -FT '{print $1 " "  $2}' /var/log/messages | grep "sftp\-server.*close " |
-   awk -v "b=$date_time_30mins_ago" -v "e=$date_time_now" -F '.' '$1 >= b && $1 <= e' >> /data/new_uploads
-   echo "***** Run:" $date_time_now "*****" >> /data/new_uploads
+   awk -v "b=$date_time_30mins_ago" -v "e=$date_time_now" -F '.' '$1 >= b && $1 <= e' >> $temp_file
+   awk -FT '{print $1 " "  $2}' $messages_file | grep "sftp\-server.*close " |
+   awk -v "b=$date_time_30mins_ago" -v "e=$date_time_now" -F '.' '$1 >= b && $1 <= e' >> $temp_file
+   echo "***** END ***** Run:" $date_time_now "*****" >> $temp_file
 else
-   awk -FT '{print $1 " "  $2}' /var/log/messages | grep "sftp\-server.*close " |
-   awk -v "b=$date_time_30mins_ago" -v "e=$date_time_now" -F '.' '$1 >= b && $1 <= e' > /data/new_uploads
-   echo "***** Run:" $date_time_now "*****" >> /data/new_uploads
+   awk -FT '{print $1 " "  $2}' $messages_file | grep "sftp\-server.*close " |
+   awk -v "b=$date_time_30mins_ago" -v "e=$date_time_now" -F '.' '$1 >= b && $1 <= e' >> $temp_file
+   echo "***** END ***** Run:" $date_time_now "*****" >> $temp_file
 fi
-
+#
 # See if there are any uploads in this run.
-
+#
 # Set up a counter to keep track of valid records.
-# Delete the output file (/data/uploads_to_process) from the previous run.
-# Check the number of lines in /data/new_uploads (2 = no new uploads, > 2 then there are uploads to process).
-# Examine /data/new_uploads line-by-line.
+# Delete the output file ($output_file) from the previous run.
+# Check the number of lines in $temp_file (2 = no new uploads, > 2 then there are uploads to process).
+# Examine $temp_file line-by-line.
 # Parse the line into variables around spaces.
 # Throw away the header and footer records ('-----' in item 0 (index origin 0).
 # For upload records:
@@ -58,16 +86,16 @@ fi
 # The last item contains the size of the file (the number of this item depends on the number of spaces in the filename).
 # Strip "s from around the filename.
 # Remove the suffix ".part" from any file names.
-# Write any records that pass the tests to /data/uploads_to_process.
-# Write a "No files to process..." record to /data/uploads_to_process if there are no valid records found.
+# Write any records that pass the tests to $output_file.
+# Write a "No files to process..." record to $output_file if there are no valid records found.
 # If files were found, then update the clamav database
-# uploads_to_process will be read by Python application.
+# $output_file will be read by Python application.
 # All issues with uploaded files will be processed and reported by the Python application.
-
-a=($(wc /data/new_uploads))
+#
+a=($(wc $temp_file))
 lines=${a[0]}
 counter=0
-rm -rf /data/uploads_to_process
+rm -rf $output_file
 if [ $lines != "2" ]; then
   while IFS='' read -r line || [[ -n "$line" ]]; do
     items=( $line )
@@ -78,15 +106,15 @@ if [ $lines != "2" ]; then
       filename=`echo "$line" | awk -F'"' '{print $2}'`
       file_name="$(sed 's|\.part$||i' <<<"$filename")"
       counter=$(($counter+1))
-      echo "Date:"${items[0]}" Time:"${items[1]}" Size:"${items[$last_word]}" File:"$file_name >> /data/uploads_to_process
+      echo "Date:"${items[0]}" Time:"${items[1]}" Size:"${items[$last_word]}" File:"$file_name >> $output_file
     fi
-  done < "/data/new_uploads"
+  done < "$temp_file"
 fi
 if [ $counter = 0 ]; then
-  echo "No files to process in this run." >> /data/uploads_to_process
+  echo "No files to process in this run." >> $output_file
 else
-  freshclam > /data/logs/clamav/clamupdate.log
+  freshclam > $clamav_log
 fi
-
+#
 # End of script (upload.sh)
-
+#
