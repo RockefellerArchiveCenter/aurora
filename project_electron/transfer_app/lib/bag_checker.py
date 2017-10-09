24cf8eb3674628ada1dc296f14343d269d118174
helrond <helrond@hotmail.com>
2017-10-09 17:49:39 -0400

    checks for datatypes

--- project_electron/transfer_app/lib/bag_checker.py
+++ project_electron/transfer_app/lib/bag_checker.py
@@ -1,7 +1,9 @@
 import bagit
 import bagit_profile
+import iso8601
 import json
 from os.path import isfile, join
+from pycountry import languages
 
 from transfer_app.lib import files_helper as FH
 from transfer_app.form import BagInfoForm
@@ -51,7 +53,7 @@ class bagChecker():
             print 'couldnt read baginfo'
             return False
 
-        if not BI_fields.has_key('BagIt_Profile_Identifier'):
+        if not 'BagIt_Profile_Identifier' in BI_fields:
             print 'No BagIt Profile to validate against'
             return False
         else:
@@ -70,6 +72,39 @@ class bagChecker():
                 return False
 
         return False
+
+    def _has_valid_datatypes(self):
+        dates = []
+        languages = []
+
+        # LOAD bag-info.txt key var
+        BI_fields = FH.get_fields_from_file('{}/{}'.format(self.archive_path,'bag-info.txt'))
+
+        if not BI_fields:
+            print 'couldnt read baginfo'
+            return False
+
+        for k,v in BI_fields.items():
+            if k in ['Date-Start', 'Date-End', 'Bagging-Date']:
+                dates.append(v)
+            if k == 'Languages':
+                languages.append(v)
+
+        if dates:
+            for date in dates.items():
+                try:
+                    iso8601.parse_date(date)
+                except:
+                    print "invalid date: %s' % date"
+                    return False
+
+        if languages:
+            for language in languages.items():
+                try:
+                    languages.lookup(language)
+                except:
+                    print "invalid language code: %s' % language"
+                    return False
 
     def _has_valid_metadata_file(self):
         # is this the right path?
@@ -96,12 +131,16 @@ class bagChecker():
 
         if not self._is_rac_bag():
             self.ecode = 'RBERR'
-            print 'didnt pass rac specs'
+            print 'didnt pass rac bag profile'
             return self.bag_failed()
-        BAGLog.log_it('PBAGP', self.archiveObj)
+
+        if not self._has_valid_datatypes():
+            self.ecode = 'RBERR'
+            print 'didnt pass rac datatype specs'
+            return self.bag_failed()
 
         if not self._has_valid_metadata_file():
-            self.ecode = 'MDERR'
+            self.ecode = 'RBERR'
             print 'optional metadata file is not valid json'
             return self.bag_failed()
         BAGLog.log_it('PBAGP', self.archiveObj)
