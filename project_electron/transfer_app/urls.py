ccb303481940a1121db6f2e44300fd74f90769e6
Hillel Arnold <helrond@hotmail.com>
2017-10-12 13:59:01 -0400

    restructures urls

--- project_electron/transfer_app/urls.py
+++ project_electron/transfer_app/urls.py
@@ -1,22 +1,8 @@
 from django.conf.urls import url
-from transfer_app.views import MainView
 from orgs.views import *
 from orgs.gviews import *
 
 urlpatterns = [
-    url(r'^$', 	MainView.as_view(), name='app_home'),
-
-    url(r'^orgs/add/$', OrganizationCreateView.as_view(), name='orgs-add'),
-    url(r'^orgs/(?P<pk>\d+)/$', OrganizationDetailView.as_view(), name='orgs-detail'),
-    url(r'^orgs/$', 	OrganizationListView.as_view(), name='orgs-list'),
-    url(r'^orgs/(?P<pk>\d+)/edit/$', OrganizationEditView.as_view(), name='orgs-edit'),
-    url(r'^orgs/(?P<pk>\d+)/transfers/$', OrganizationTransfersView.as_view(), name='orgs-transfers-report'),
-
-    url(r'^users/$', UsersListView.as_view(), name='users-list'),
-    url(r'^users/add/$', UsersCreateView.as_view(), name='users-add'),
-    url(r'^users/by/(?P<page_type>(unassigned|company))/$', UsersListView.as_view(), name='users-list-by'),
-    url(r'^users/(?P<pk>\d+)/$', UsersDetailView.as_view(), name='users-detail'),
-    url(r'^users/(?P<pk>\d+)/edit/$', UsersEditView.as_view(), name='users-edit'),
 
     url(r'^transfers/(?P<pk>\d+)$', TransferDetailView.as_view(), name='transfer-detail'),
 
