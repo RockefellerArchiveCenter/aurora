e5d2ec65c3a25abafba08a066bedf7eeff27a752
Hillel Arnold <helrond@hotmail.com>
2017-10-12 13:59:01 -0400

    restructures urls

--- project_electron/transfer_app/urls.py
+++ project_electron/transfer_app/urls.py
@@ -3,6 +3,8 @@ from orgs.views import *
 from transfer_app.views import *
 
 urlpatterns = [
-    url(r'^$', RecentTransfersView.as_view(), name='org-transfers'),
-    url(r'^(?P<pk>\d+)$', TransferDetailView.as_view(), name='transfer-detail'),
+    url(r'^transfers/(?P<pk>\d+)$', TransferDetailView.as_view(), name='transfer-detail'),
+
+    url(r'^transfers/$', OrgTransfersView.as_view(), name='org-transfers'),
+    url(r'^my-transfers/$', MyTransfersView.as_view(), name='org-my-transfers'),
 ]
