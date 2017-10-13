9092114a932fcd80f294bb1aff93d2f1c96b6a1c
Hillel Arnold <helrond@hotmail.com>
2017-10-13 16:25:52 -0400

    adding data into appraisal dashboard

--- project_electron/appraise/views.py
+++ project_electron/appraise/views.py
@@ -3,7 +3,16 @@ from __future__ import unicode_literals
 from django.views.generic import TemplateView
 
 from django.shortcuts import render
+from orgs.models import Archives
+from orgs.authmixins import LoggedInMixinDefaults
 
 
 class AppraiseView(TemplateView):
     template_name = "appraise/main.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(TemplateView, self).get_context_data(**kwargs)
+
+        # This should filter by transfer status too
+        context['uploads'] = Archives.objects.filter(organization = self.request.user.organization).order_by('created_time')
+        return context
