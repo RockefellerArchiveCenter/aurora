ef3d75c64b45cb387ce2d73d54a850c4a021d966
Hillel Arnold <helrond@hotmail.com>
2017-10-14 17:30:26 -0400

    reorganizing views and templates

--- project_electron/transfer_app/urls.py
+++ project_electron/transfer_app/urls.py
@@ -3,8 +3,6 @@ from orgs.views import *
 from transfer_app.views import *
 
 urlpatterns = [
-    url(r'^transfers/(?P<pk>\d+)$', TransferDetailView.as_view(), name='transfer-detail'),
-
-    url(r'^transfers/$', OrgTransfersView.as_view(), name='org-transfers'),
-    url(r'^my-transfers/$', MyTransfersView.as_view(), name='org-my-transfers'),
+    url(r'^$', RecentTransfersView.as_view(), name='org-transfers'),
+    url(r'^(?P<pk>\d+)$', TransferDetailView.as_view(), name='transfer-detail'),
 ]
