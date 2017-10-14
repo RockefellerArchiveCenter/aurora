d51c4a7e918edd598edd8e8ef95f11f75044e5ba
Hillel Arnold <helrond@hotmail.com>
2017-10-14 17:30:26 -0400

    reorganizing views and templates

--- project_electron/transfer_app/urls.py
+++ project_electron/transfer_app/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import url
 from orgs.views import *
-from orgs.gviews import *
+from transfer_app.views import *
 
 urlpatterns = [
-
-    url(r'^transfers/(?P<pk>\d+)$', TransferDetailView.as_view(), name='transfer-detail'),
-
-    url(r'^transfers/$', OrgTransfersView.as_view(), name='org-transfers'),
-    url(r'^my-transfers/$', MyTransfersView.as_view(), name='org-my-transfers'),
+    url(r'^$', RecentTransfersView.as_view(), name='org-transfers'),
+    url(r'^(?P<pk>\d+)$', TransferDetailView.as_view(), name='transfer-detail'),
 ]
--- project_electron/transfer_app/views.py
+++ project_electron/transfer_app/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View, DetailView
 
 from django.shortcuts import render
 from orgs.models import Archives
@@ -16,3 +16,19 @@ class MainView(LoggedInMixinDefaults, TemplateView):
         context['uploads'] = Archives.objects.filter(organization = self.request.user.organization).order_by('-created_time')[:15]
         context['uploads_count'] = Archives.objects.filter(organization = self.request.user.organization).count()
         return context
+
+class RecentTransfersView(LoggedInMixinDefaults, View):
+    template_name = 'orgs/recent_transfers.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {
+            'page_title' : 'Organization Transfers',
+            'org_uploads' : Archives.objects.filter(organization = request.user.organization).order_by('-created_time')[:25],
+            'org_uploads_count' : Archives.objects.filter(organization = request.user.organization).count(),
+            'user_uploads' : Archives.objects.filter(organization = request.user.organization, user_uploaded=request.user).order_by('-created_time')[:25],
+            'user_uploads_count' : Archives.objects.filter(organization = request.user.organization, user_uploaded = request.user).count(),
+        })
+
+class TransferDetailView(DetailView):
+    template_name = 'transfer_app/transfer_detail.html'
+    model = Archives
