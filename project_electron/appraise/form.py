0dd27ec16aa3708627538effb733d5a635ebda68
Hillel Arnold <helrond@hotmail.com>
2017-11-21 09:11:22 -0500

    appraisal note as separate button

--- project_electron/appraise/form.py
+++ project_electron/appraise/form.py
@@ -5,3 +5,8 @@ class AppraisalNoteUpdateForm(forms.ModelForm):
     class Meta:
         model = Archives
         fields = ['appraisal_note']
+
+    def __init__(self, *args, **kwargs):
+        form = super(AppraisalNoteUpdateForm, self).__init__(*args, **kwargs)
+        for visible in form.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
--- project_electron/appraise/urls.py
+++ project_electron/appraise/urls.py
@@ -4,6 +4,6 @@ from appraise.views import AppraiseView, AppraisalNoteUpdateView
 urlpatterns = [
 
     url(r'^$', 	AppraiseView.as_view(), name='appraise-main'),
-    url(r'^accept/(?P<pk>\d+)/$', AppraisalNoteUpdateView.as_view(), name='accept-transfer'),
+    url(r'^(?P<pk>\d+)/note/$', AppraisalNoteUpdateView.as_view(), name='appraise-note'),
 
 ]
