d9c193c1ee0e412b7aa186c79049df3e14ce4d7c
Hillel Arnold <helrond@hotmail.com>
2017-11-20 23:13:11 -0500

    adds accept url

--- project_electron/appraise/urls.py
+++ project_electron/appraise/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from appraise.views import AppraiseView
+from appraise.views import AppraiseView, AppraisalNoteUpdateView
 
 urlpatterns = [
 
     url(r'^$', 	AppraiseView.as_view(), name='appraise-main'),
+    url(r'^accept/(?P<pk>\d+)/$', AppraisalNoteUpdateView.as_view(), name='accept-transfer'),
 
 ]
