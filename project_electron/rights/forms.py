4b498e909f803fc6420cf6168ed2e6cd858daed6
Hillel Arnold <helrond@hotmail.com>
2017-12-02 13:43:28 -0500

    stashing changes

--- project_electron/rights/forms.py
+++ project_electron/rights/forms.py
@@ -7,4 +7,63 @@ from rights.models import *
 class RightsForm(forms.ModelForm):
 	class Meta:
 		model = RightsStatement
-		exclude = []
+		fields = ('rightsbasis', 'appliestotype')
+        widgets = {
+            'rightsbasis': forms.TextInput(attrs={'class': 'form-control',}),
+			'appliestotype': forms.CheckboxInput(attrs={'class': 'form-control',}),}
+
+class RightsGrantedForm(forms.ModelForm):
+    class Meta:
+        model = RightsStatementRightsGranted
+        fields = ('act', 'startdate', 'enddate', 'enddateopen', 'rightsgrantednote')
+        widgets = {
+            'act': forms.TextInput(attrs={'class': 'form-control', 'title': "The action the preservation repository is allowed to take; eg replicate, migrate, modify, use, disseminate, delete"}),
+            'restriction': forms.TextInput(attrs={'class': 'form-control',}),
+            'startdate': forms.TextInput(attrs={'class': 'form-control', 'title': "The beginning date of the rights or restrictions granted"}),
+            'enddate': forms.TextInput(attrs={'class': 'form-control', 'title': "The ending date of the rights or restrictions granted"}),
+            'enddateopen': forms.CheckboxInput(attrs={'class': 'form-control', 'title': 'Use "OPEN" for an open ended term of restriction. Omit end date if the ending date is unknown or the permission statement applies to many objects with different end dates.'}),
+			'rightsgrantednote': forms.TextInput(attrs={'class': 'form-control',}), }
+
+class RightsCopyrightForm(forms.ModelForm):
+    class Meta:
+        model = RightsStatementCopyright
+        fields = ('copyrightstatus', 'copyrightjurisdiction', 'copyrightstatusdeterminationdate', 'copyrightapplicablestartdate', 'copyrightapplicableenddate', 'copyrightenddateopen', 'copyrightnote')
+        widgets = {
+            'copyrightstatus': forms.widgets.Select(attrs={'class': 'form-control', 'title': "A coded designation of the copyright status of the object at the time the rights statement is recorded. Available options: Copyrighted, Public Domain, Unknown"}),
+            'copyrightjurisdiction': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The country whose copyright laws apply [ISO 3166]"}),
+            'copyrightstatusdeterminationdate': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The date that the copyright status recorded in 'copyright status' was determined."}),
+            'copyrightapplicablestartdate': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The date when the particular copyright applies or is applied to the content."}),
+            'copyrightapplicableenddate': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The date when the particular copyright no longer applies or is applied to the content."}),
+			'copyrightnote': forms.widgets.Textarea(attrs={'class': 'form-control',}), }
+
+class RightsStatuteForm(forms.ModelForm):
+    class Meta:
+        model = RightsStatementStatuteInformation
+        fields = ('statutejurisdiction', 'statutecitation', 'statutedeterminationdate', 'statuteapplicablestartdate', 'statuteapplicableenddate', 'statuteenddateopen', 'statutenote')
+        widgets = {
+            'statutejurisdiction': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The country or other political body enacting the statute."}),
+            'statutecitation': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "An identifying designation for the statute."}),
+            'statutedeterminationdate': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The date that the determination was made that the statue authorized the permission(s) noted."}),
+            'statuteapplicablestartdate': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The date when the statute begins to apply or is applied to the content."}),
+            'statuteapplicableenddate': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The date when the statute ceasees to apply or be applied to the content."}),
+			'statutenote': forms.widgets.Textarea(attrs={'class': 'form-control',}), }
+
+class RightsOtherRightsForm(forms.ModelForm):
+    class Meta:
+        model = RightsStatementOtherRightsInformation
+        fields = ('otherrightsbasis', 'otherrightsapplicablestartdate', 'otherrightsapplicableenddate', 'otherrightsenddateopen', 'otherrightsnote')
+        widgets = {
+            'otherrightsbasis': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The designation of the basis for the other right or permission described in the rights statement identifier."}),
+            'otherrightsapplicablestartdate': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The date when the other right applies or is applied to the content."}),
+            'otherrightsapplicableenddate': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The date when the other right no longer applies or is applied to the content."}),
+			'otherrightsnote': forms.widgets.Textarea(attrs={'class': 'form-control',}), }
+
+class RightsLicenseForm(forms.ModelForm):
+    class Meta:
+        model = RightsStatementLicense
+        fields = ('licenseterms', 'licenseapplicablestartdate', 'licenseapplicableenddate', 'licenseenddateopen', 'licensenote')
+        widgets = {
+            'licenseterms': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "Text describing the license or agreement by which permission as granted."}),
+            'licenseapplicablestartdate': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The date at which the license first applies or is applied to the content."}),
+            'licenseapplicableenddate': forms.widgets.TextInput(attrs={'class': 'form-control', 'title': "The end date at which the license no longer applies or is applied to the content."}),
+			'licensenote': forms.widgets.Textarea(attrs={'class': 'form-control',}), }
--- project_electron/rights/models.py
+++ project_electron/rights/models.py
@@ -4,11 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 from orgs.models import Organization, Archives
 
-# Create your models here.
-
 class RightsStatement(models.Model):
     organization = models.ForeignKey(Organization)
     archive = models.ForeignKey(Archives, null=True, blank=True)
+    APPLIES_TO_TYPE_CHOICES = ( # Eventually this should be replaced by a call to get record types associated with this organization
+        ('administrative records', 'Administrative Records'),
+        ('annual reports', 'Annual Reports'),
+        ('board materials', 'Board Materials'),
+        ('communications and publications', 'Communications and Publications'),
+        ('grant records', 'Grant Records'),
+    )
+    appliestotype = models.CharField(choices=APPLIES_TO_TYPE_CHOICES, max_length=100)
     RIGHTS_BASIS_CHOICES = (
         ('Copyright', 'Copyright'),
         ('Statute', 'Statute'),
@@ -26,45 +32,61 @@ class RightsStatementCopyright(models.Model):
         ('public domain', 'public domain'),
         ('unknown', 'unknown'),
     )
-    copyrightstatus = models.TextField(choices=PREMIS_COPYRIGHT_STATUSES, default='unknown')
-    copyrightjurisdiction = models.TextField()
-    copyrightstatusdeterminationdate = models.TextField(blank=True, null=True)
-    copyrightapplicablestartdate = models.TextField(blank=True, null=True)
-    copyrightapplicableenddate = models.TextField(blank=True, null=True)
+    copyrightstatus = models.CharField(choices=PREMIS_COPYRIGHT_STATUSES, default='unknown', max_length=64)
+    copyrightjurisdiction = models.CharField(max_length=2)
+    copyrightexpirationperiod = models.PositiveSmallIntegerField()
+    copyrightstatusdeterminationdate = models.DateField(blank=True, null=True)
+    copyrightapplicablestartdate = models.DateField(blank=True, null=True)
+    copyrightapplicableenddate = models.DateField(blank=True, null=True)
     copyrightenddateopen = models.BooleanField(default=False)
     copyrightnote = models.TextField()
 
 class RightsStatementLicense(models.Model):
     rightsstatement = models.ForeignKey(RightsStatement)
     licenseterms = models.TextField(blank=True, null=True)
-    licenseapplicablestartdate = models.TextField(blank=True, null=True)
-    licenseapplicableenddate = models.TextField(blank=True, null=True)
+    licenseapplicablestartdate = models.DateField(blank=True, null=True)
+    licenseapplicableenddate = models.DateField(blank=True, null=True)
     licenseenddateopen = models.BooleanField(default=False)
     licensenote = models.TextField()
 
 class RightsStatementRightsGranted(models.Model):
     rightsstatement = models.ForeignKey(RightsStatement)
-    act = models.TextField()
-    startdate = models.TextField(blank=True, null=True)
-    enddate = models.TextField(blank=True, null=True)
+    ACT_CHOICES = (
+        ('publish', 'Publish'),
+        ('disseminate', 'Disseminate'),
+        ('replicate', 'Replicate'),
+        ('migrate', 'Migrate'),
+        ('modify', 'Modify'),
+        ('use', 'Use'),
+        ('delete', 'Delete'),
+    )
+    act = models.CharField(choices=ACT_CHOICES, max_length=64)
+    startdate = models.DateField(blank=True, null=True)
+    enddate = models.DateField(blank=True, null=True)
     enddateopen = models.BooleanField(default=False)
     rightsgrantednote = models.TextField()
-    restriction = models.TextField()
+    RESTRICTION_CHOICES = (
+        ('allow', 'Allow'),
+        ('disallow', 'Disallow'),
+        ('conditional', 'Conditional'),
+    )
+    restriction = models.CharField(choices=RESTRICTION_CHOICES, max_length=64)
 
 class RightsStatementStatuteInformation(models.Model):
     rightsstatement = models.ForeignKey(RightsStatement)
-    statutejurisdiction = models.TextField()
+    statutejurisdiction = models.CharField(max_length=2)
     statutecitation = models.TextField()
-    statutedeterminationdate = models.TextField(blank=True, null=True)
-    statuteapplicablestartdate = models.TextField(blank=True, null=True)
-    statuteapplicableenddate = models.TextField(blank=True, null=True)
+    statutedeterminationdate = models.DateField(blank=True, null=True)
+    statuteapplicablestartdate = models.DateField(blank=True, null=True)
+    statuteapplicableenddate = models.DateField(blank=True, null=True)
     statuteenddateopen = models.BooleanField(default=False)
     statutenote = models.TextField()
 
 class RightsStatementOtherRightsInformation(models.Model):
     rightsstatement = models.ForeignKey(RightsStatement)
-    otherrightsbasis = models.TextField(default='Donor')
-    otherrightsapplicablestartdate = models.TextField(blank=True, null=True)
-    otherrightsapplicableenddate = models.TextField(blank=True, null=True)
+    otherrightsbasis = models.CharField(max_length=64, default='Donor')
+    donorembargoperiod = models.PositiveSmallIntegerField()
+    otherrightsapplicablestartdate = models.DateField(blank=True, null=True)
+    otherrightsapplicableenddate = models.DateField(blank=True, null=True)
     otherrightsenddateopen = models.BooleanField(default=False)
     otherrightsnote = models.TextField()
