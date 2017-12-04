2ebc8fc22bd032646d6b944aa6c5ce69202fd77e
Hillel Arnold <helrond@hotmail.com>
2017-12-04 15:24:38 -0500

    everyone should be able to see rights info, but not edit it

--- project_electron/rights/views.py
+++ project_electron/rights/views.py
@@ -14,7 +14,7 @@ class RightsCreateView(RACAdminMixin, CreateView):
     model = RightsStatement
     form_class = RightsForm
 
-class RightsDetailView(RACAdminMixin, DetailView):
+class RightsDetailView(DetailView):
     template_name = 'rights/detail.html'
     model = RightsStatement
 
