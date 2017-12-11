44ae2c150e3f62405f9b4c8f948a593fd6f19594
Hillel Arnold <helrond@hotmail.com>
2017-12-11 14:34:52 -0500

    stashing

--- project_electron/accession/models.py
+++ project_electron/accession/models.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Accession(models.Model):
+    title =             models.CharField(max_length=256)
+    accession_number =  models.CharField(max_length=10)
+    accession_date =    models.DateTimeField(auto_now_add=True)
+    start_date =        models.DateTimeField()
+    end_date =          models.DateTimeField()
+    extent_files =      models.PositiveIntegerField()
+    extent_size =       models.PositiveIntegerField()
+    # creators =          models.ManyToManyField(RecordCreators)
+    description =       models.TextField()
+    access_restrictions =   models.TextField()
+    use_restrictions =      models.TextField()
+    resource =              models.URLField()
+    acquisition_type =      models.CharField(max_length=200)
+    appraisal_note =        models.TextField()
