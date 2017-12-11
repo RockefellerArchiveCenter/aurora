dc46370711b8371a802d7bae020d94267573cdcc
Darnell Lynch <darnell.lynch@marist.edu>
2017-12-11 14:13:23 -0500

    using AJAX add/edit note update; doesnt orphan a page

--- project_electron/appraise/urls.py
+++ project_electron/appraise/urls.py
@@ -4,6 +4,6 @@ from appraise.views import AppraiseView, AppraisalNoteUpdateView
 urlpatterns = [
 
     url(r'^$', 	AppraiseView.as_view(), name='appraise-main'),
-    url(r'^(?P<pk>\d+)/note/$', AppraisalNoteUpdateView.as_view(), name='appraise-note'),
+    # url(r'^(?P<pk>\d+)/note/$', AppraisalNoteUpdateView.as_view(), name='appraise-note'),
 
 ]
