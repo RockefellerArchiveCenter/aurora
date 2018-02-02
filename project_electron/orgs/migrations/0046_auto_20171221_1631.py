4782b58927194c01c7320f3f826ca0e7f2b3996a
catmurphy <catherine.murphy@marist.edu>
2018-02-02 16:44:41 -0500

    Update and rename 0045_auto_20171221_1631.py to 0046_auto_20171221_1631.py

--- project_electron/orgs/migrations/0046_auto_20171221_1631.py
+++ project_electron/orgs/migrations/0046_auto_20171221_1631.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('orgs', '0044_auto_20171128_1355'),
+        ('orgs', '0045_organization_acquisition_type'),
     ]
 
     operations = [
