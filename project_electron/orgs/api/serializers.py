7100fa5001a0d0970d97375c68ff3a38c990b745
Hillel Arnold <helrond@hotmail.com>
2018-02-08 21:48:08 -0500

    basic api done, need authentication next

--- project_electron/orgs/api/serializers.py
+++ project_electron/orgs/api/serializers.py
@@ -5,47 +5,40 @@ from rest_framework import serializers
 from orgs.models import Organization, Archives, BAGLog, BagInfoMetadata
 
 class BAGLogSerializer(serializers.HyperlinkedModelSerializer):
+	code = serializers.StringRelatedField()
+
 	class Meta:
 		model = BAGLog
-		fields = ('code', 'archive', 'log_info')
+		fields = ('url', 'code', 'archive', 'log_info', 'created_time')
 
 class BagInfoMetadataSerializer(serializers.HyperlinkedModelSerializer):
+	source_organization = serializers.StringRelatedField()
+	language = serializers.StringRelatedField(many=True)
+	record_creators = serializers.StringRelatedField(many=True)
+
 	class Meta:
 		model = BagInfoMetadata
-		fields = '__all__'
+		fields = ('url', 'source_organization', 'external_identifier',
+					'title', 'record_creators', 'internal_sender_description',
+					'date_start', 'date_end', 'record_type', 'language',
+					'bag_count', 'bag_group_identifier', 'payload_oxum',
+					'bagit_profile_identifier', 'bagging_date',)
 
 class ArchivesSerializer(serializers.HyperlinkedModelSerializer):
-	notifications = serializers.HyperlinkedIdentityField(view_name='archives-notifications')
 	metadata = serializers.HyperlinkedIdentityField(view_name='archives-metadata')
+	notifications = serializers.HyperlinkedIdentityField(view_name='archives-notifications')
 
 	class Meta:
 		model = Archives
-		fields = ('organization', 'bag_it_name', 'process_status', 'metadata', 'notifications')
-
-		@list_route()
-	    def notifications(self, request, *args, **kwargs):
-	        org = self.get_object()
-			notifications = BAGLog.objects.filter(organization=org).order_by('created_time')
-			notifications_json = BAGLogSerializer(notifications, many=True)
-	        return Response(notifications_json.data)
-
-		@list_route()
-	    def metadata(self, request, *args, **kwargs):
-	        org = self.get_object()
-			metadata = BagInfoMetadata.objects.get(source_organization=org)
-			metadata_json = BAGLogSerializer(notifications, many=True)
-	        return Response(metadata_json.data)
+		fields = ('url', 'organization', 'bag_it_name', 'process_status', 'machine_file_size',
+		'machine_file_upload_time', 'machine_file_identifier', 'machine_file_type',
+		'created_time', 'modified_time', 'appraisal_note', 'additional_error_info',
+		'metadata', 'notifications',)
 
 class OrganizationSerializer(serializers.HyperlinkedModelSerializer):
 	transfers = serializers.HyperlinkedIdentityField(view_name='organization-transfers')
+	notifications = serializers.HyperlinkedIdentityField(view_name='organization-notifications')
 
 	class Meta:
 		model = Organization
-		fields = ('url', 'id', 'is_active', 'name', 'machine_name', 'acquisition_type', 'transfers')
-
-	@list_route()
-    def transfers(self, request, *args, **kwargs):
-        org = self.get_object()
-		transfers = Archives.objects.filter(organization=org).order_by('-created_time')
-		transfer_json = ArchivesSerializer(transfers, many=True)
-        return Response(transfer_json.data)
+		fields = ('url', 'id', 'is_active', 'name', 'machine_name', 'acquisition_type', 'transfers', 'notifications')
--- project_electron/orgs/api/views.py
+++ project_electron/orgs/api/views.py
@@ -5,16 +5,53 @@ from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 
-from orgs.models import Organization, Archives, BAGLog
-from orgs.api.serializers import OrganizationSerializer, ArchivesSerializer, BAGLogSerializer
+from orgs.models import Organization, Archives, BAGLog, BagInfoMetadata
+from orgs.api.serializers import OrganizationSerializer, ArchivesSerializer, BAGLogSerializer, BagInfoMetadataSerializer
 
 class OrganizationViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
 
+    @detail_route()
+    def transfers(self, request, *args, **kwargs):
+        org = self.get_object()
+        transfers = Archives.objects.filter(organization=org).order_by('-created_time')
+        page = self.paginate_queryset(transfers)
+        if page is not None:
+            serializer = ArchivesSerializer(page, context={'request': request}, many=True)
+            return self.get_paginated_response(serializer.data)
+        return Response(serializer.data)
+
+        return Response(transfer_json.data)
+
+    @detail_route()
+    def notifications(self, request, *args, **kwargs):
+        org = self.get_object()
+        archives = Archives.objects.filter(organization=org)
+        notifications = BAGLog.objects.filter(archive__in=archives).order_by('-created_time')
+        page = self.paginate_queryset(notifications)
+        if page is not None:
+            serializer = BAGLogSerializer(page, context={'request': request}, many=True)
+            return self.get_paginated_response(serializer.data)
+        return Response(serializer.data)
+
 class ArchivesViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Archives.objects.all()
     serializer_class = ArchivesSerializer
+
+    @detail_route()
+    def notifications(self, request, *args, **kwargs):
+        archive = self.get_object()
+        notifications = BAGLog.objects.filter(archive=archive).order_by('created_time')
+        notifications_json = BAGLogSerializer(notifications, context={'request': request}, many=True)
+        return Response(notifications_json.data)
+
+    @detail_route()
+    def metadata(self, request, *args, **kwargs):
+        archive = self.get_object()
+        metadata = BagInfoMetadata.objects.filter(archive=archive)
+        metadata_json = BagInfoMetadataSerializer(metadata, context={'request': request}, many=True)
+        return Response(metadata_json.data)
 
 class BAGLogViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = BAGLog.objects.all()
