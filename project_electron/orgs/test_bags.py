b734d2eb2b5e8bdea069b2d582e1c588db1df95b
Hillel Arnold <helrond@hotmail.com>
2018-02-23 09:10:02 -0500

    updating gitignore, adding travis.yml, refactoring of setup tasks

--- project_electron/orgs/test_bags.py
+++ project_electron/orgs/test_bags.py
@@ -5,22 +5,19 @@ from os import path, makedirs
 from django.test import TestCase
 from project_electron import config
 from orgs.models import Archives
+from orgs.test_setup import SetupTest
 from transfer_app.lib.bag_checker import bagChecker
-from orgs.test_setup import *
 
 
 class BagTest(TestCase):
     def setUp(self):
-        if path.isdir(config.TESTING_TMP_DIR):
-            FH.remove_file_or_dir(config.TESTING_TMP_DIR)
-        else:
-            makedirs(config.TESTING_TMP_DIR)
+        SetupTest.setup_tmp_dir()
 
     def tearDown(self):
-        FH.remove_file_or_dir(config.TESTING_TMP_DIR)
+        SetupTest.remove_tmp_dir()
 
     def test_large_bag(self):
-        """Bag larger than size limit is rejected"""
+        '''Bag larger than size limit is rejected'''
         pass
         # Given a bag size exceeds size limit
         # When the validations script is run
@@ -30,7 +27,7 @@ class BagTest(TestCase):
         # 	And error notifications are delivered to client
 
     def test_invalid_filename(self):
-        """Bag with special characters in filename is rejected"""
+        '''Bag with special characters in filename is rejected'''
         pass
         # Given a bag has a special characters in a filename
         # When the validations script is run
@@ -40,7 +37,7 @@ class BagTest(TestCase):
         # 	And error notifications are delivered to client
 
     def test_virus_check(self):
-        """Bag with a virus is rejected"""
+        '''Bag with a virus is rejected'''
         pass
         # Given a bag contains a virus
         # When virus check script is run
@@ -52,7 +49,7 @@ class BagTest(TestCase):
         # 	And Marist sysadmins are notified
 
     def test_update_definitions(self):
-        """Virus definitions are updated before script is run"""
+        '''Virus definitions are updated before script is run'''
         pass
         # Given virus definitions are outdated
         # When virus check script is triggered
@@ -60,119 +57,119 @@ class BagTest(TestCase):
         # 	And the virus check script is run
 
     def test_missing_bag_declaration(self):
-        """Bag missing bag declaration is rejected"""
+        '''Bag missing bag declaration is rejected'''
         bag_names = ['missing_bag_declaration', 'missing_bag_declaration.zip',
                      'missing_bag_declaration.tar', 'missing_bag_declaration.tar.gz']
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertFalse(bag.bag_passed_all())
 
     def test_missing_payload_manifest(self):
-        """Bag missing payload manifest is rejected"""
+        '''Bag missing payload manifest is rejected'''
         bag_names = ['missing_payload_manifest', 'missing_payload_manifest.zip',
                      'missing_payload_manifest.tar', 'missing_payload_manifest.tar.gz']
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertFalse(bag.bag_passed_all())
 
     def test_missing_bag_manifest(self):
-        """Bag missing bag manifest is rejected"""
+        '''Bag missing bag manifest is rejected'''
         bag_names = ['missing_bag_manifest', 'missing_bag_manifest.zip',
                      'missing_bag_manifest.tar', 'missing_bag_manifest.tar.gz']
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertFalse(bag.bag_passed_all())
 
     def test_missing_payload_directory(self):
-        """Bag missing payload directory is rejected"""
+        '''Bag missing payload directory is rejected'''
         bag_names = ['missing_payload_directory', 'missing_payload_directory.zip',
                      'missing_payload_directory.tar', 'missing_payload_directory.tar.gz']
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertFalse(bag.bag_passed_all())
 
     def test_empty_payload_directory(self):
         # TODO: According to the bagit spec, it seems that empty payload directories are technically allowed.
-        """Bag with empty payload directory is not rejected"""
+        '''Bag with empty payload directory is not rejected'''
         bag_names = ['empty_payload_directory', 'empty_payload_directory.zip',
                      'empty_payload_directory.tar', 'empty_payload_directory.tar.gz']
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertTrue(bag.bag_passed_all())
 
     def test_changed_file(self):
-        """Bag with a changed file is rejected"""
+        '''Bag with a changed file is rejected'''
         bag_names = ['changed_file', 'changed_file.zip',
                      'changed_file.tar', 'changed_file.tar.gz']
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertFalse(bag.bag_passed_all())
 
     def test_invalid_metadata_file(self):
-        """Bag with invalid metadata.json file is rejected"""
+        '''Bag with invalid metadata.json file is rejected'''
         bag_names = ['invalid_metadata_file', 'invalid_metadata_file.zip',
                      'invalid_metadata_file.tar', 'invalid_metadata_file.tar.gz']
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertFalse(bag.bag_passed_all())
 
     def test_no_metadata_file(self):
-        """Bag with invalid metadata.json file is rejected"""
+        '''Bag with invalid metadata.json file is rejected'''
         bag_names = ['no_metadata_file', 'no_metadata_file.zip',
                      'no_metadata_file.tar', 'no_metadata_file.tar.gz']
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertTrue(bag.bag_passed_all())
 
     def test_missing_required_fields(self):
-        """Bag with missing fields in bag-info.txt is rejected"""
+        '''Bag with missing fields in bag-info.txt is rejected'''
         bag_names = ['missing_description', 'missing_description.zip', 'missing_description.tar', 'missing_description.tar.gz', 'missing_record_type', 'missing_record_type.zip', 'missing_record_type.tar', 'missing_record_type.tar.gz',
                      'missing_source_organization', 'missing_source_organization.zip', 'missing_source_organization.tar', 'missing_source_organization.tar.gz', 'missing_title', 'missing_title.zip', 'missing_title.tar', 'missing_title.tar.gz', ]
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertFalse(bag.bag_passed_all())
 
     def test_repeating_fields(self):
-        """Bag containing repeating non-repeatable metadata fields in bag-info.txt is rejected"""
+        '''Bag containing repeating non-repeatable metadata fields in bag-info.txt is rejected'''
         bag_names = ['repeating_record_type', 'repeating_record_type.zip', 'repeating_record_type.tar', 'repeating_record_type.tar.gz', 'repeating_source_organization', 'repeating_source_organization.zip',
                      'repeating_source_organization.tar', 'repeating_source_organization.tar.gz', 'repeating_title', 'repeating_title.zip', 'repeating_title.tar', 'repeating_title.tar.gz', ]
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertFalse(bag.bag_passed_all())
 
     def test_invalid_data_type(self):
-        """A bag containing a metadata element that does not conform to datatype specification is rejected"""
+        '''A bag containing a metadata element that does not conform to datatype specification is rejected'''
         bag_names = ['invalid_datatype_date', 'invalid_datatype_date.zip', 'invalid_datatype_date.tar', 'invalid_datatype_date.tar.gz',
                          'invalid_datatype_language', 'invalid_datatype_language.zip', 'invalid_datatype_language.tar', 'invalid_datatype_language.tar.gz']
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertFalse(bag.bag_passed_all())
 
     def test_unauthorized_term(self):
-        """Bag containing metadata elements which do not adhere to locally controlled vocabularies is rejected"""
+        '''Bag containing metadata elements which do not adhere to locally controlled vocabularies is rejected'''
         bag_names = ['unauthorized_record_type', 'unauthorized_record_type.zip', 'unauthorized_record_type.tar', 'unauthorized_record_type.tar.gz',
                      'unauthorized_source_organization', 'unauthorized_source_organization.zip', 'unauthorized_source_organization.tar', 'unauthorized_source_organization.tar.gz']
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertFalse(bag.bag_passed_all())
 
     def test_valid_bag(self):
-        """Bag which passes all checks is stored"""
+        '''Bag which passes all checks is stored'''
         bag_names = ['valid_bag', 'valid_bag.zip',
                      'valid_bag.tar', 'valid_bag.tar.gz']
         for name in bag_names:
-            archive = set_up_archive_object('test_bags/{}'.format(name))
+            archive = SetupTest.create_test_archive('test_bags/{}'.format(name))
             bag = bagChecker(archive)
             self.assertTrue(bag.bag_passed_all())
--- project_electron/orgs/test_setup.py
+++ project_electron/orgs/test_setup.py
@@ -4,44 +4,55 @@ from __future__ import unicode_literals
 from datetime import datetime
 from os.path import splitext
 from orgs.models import Archives, Organization, User
-from project_electron import config
+from project_electron import config, settings
 from transfer_app.lib import files_helper as FH
 
-def create_test_org():
-    test_org = Organization.objects.get_or_create(name='Ford Foundation', machine_name='org1')
-    print 'Test organization {} created'.format(test_org)
-    return test_org[0]
+class SetupTest():
 
-def create_test_user(org):
-    test_user = User(organization=org).save()
-    print 'Test user created'
-    return test_user
+    def setup_tmp_dir(self):
+        if path.isdir(config.TESTING_TMP_DIR):
+            FH.remove_file_or_dir(config.TESTING_TMP_DIR)
+        else:
+            makedirs(config.TESTING_TMP_DIR)
 
-def set_up_archive_object(bag_name):
-    org = create_test_org()
-    # user = self.create_test_user(org)
-    bag_file_path = "{}{}".format(config.PROJECT_ROOT_DIR, bag_name)
-    # TODO: this should be replaced with calls to functions, but will require some refactoring
-    extension = splitext(bag_file_path)
-    tar_accepted_ext = ['.gz', '.tar']
-    if extension[-1] in tar_accepted_ext:
-        file_type = 'TAR'
-    elif extension[-1] == '.zip':
-        file_type = 'ZIP'
-    else:
-        file_type = 'OTHER'
+    def remove_tmp_dir(self):
+        FH.remove_file_or_dir(config.TESTING_TMP_DIR)
 
-    archive = Archives(
-        organization = org,
-        # user_uploaded = user,
-        machine_file_path = bag_file_path,
-        machine_file_size =     FH.file_get_size(bag_file_path, file_type),
-        # TODO: fix naive datetime warning
-        machine_file_upload_time =  FH.file_modified_time(bag_file_path),
-        machine_file_identifier =   "{}{}{}".format(bag_name,org,datetime.now()),
-        machine_file_type       =   file_type,
-        bag_it_name =               (bag_file_path.split('/')[-1]).split('.')[0],
-    )
-    archive.save()
-    print "Archive {} saved!".format(archive)
-    return archive
+    def create_test_org(self):
+        test_org = Organization.objects.get_or_create(name='Ford Foundation', machine_name='org1')
+        print 'Test organization {} created'.format(test_org)
+        return test_org[0]
+
+    def create_test_user(self, org):
+        test_user = User(organization=org).save()
+        print 'Test user created'
+        return test_user
+
+    def create_test_archive(self, bag_name):
+        org = create_test_org()
+        # user = self.create_test_user(org)
+        bag_file_path = "{}{}".format(settings.BASE_DIR, bag_name)
+        # TODO: this should be replaced with calls to functions, but will require some refactoring
+        extension = splitext(bag_file_path)
+        tar_accepted_ext = ['.gz', '.tar']
+        if extension[-1] in tar_accepted_ext:
+            file_type = 'TAR'
+        elif extension[-1] == '.zip':
+            file_type = 'ZIP'
+        else:
+            file_type = 'OTHER'
+
+        archive = Archives(
+            organization = org,
+            # user_uploaded = user,
+            machine_file_path = bag_file_path,
+            machine_file_size =     FH.file_get_size(bag_file_path, file_type),
+            # TODO: fix naive datetime warning
+            machine_file_upload_time =  FH.file_modified_time(bag_file_path),
+            machine_file_identifier =   "{}{}{}".format(bag_name,org,datetime.now()),
+            machine_file_type       =   file_type,
+            bag_it_name =               (bag_file_path.split('/')[-1]).split('.')[0],
+        )
+        archive.save()
+        print "Archive {} saved!".format(archive)
+        return archive
