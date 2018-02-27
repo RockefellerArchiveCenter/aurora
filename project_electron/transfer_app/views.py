b6f07462f71bc79a29860614bd0498504d1d9b73
Hillel Arnold <helrond@hotmail.com>
2018-02-26 22:31:17 -0500

    if user is archivist, show all transfers

--- project_electron/transfer_app/views.py
+++ project_electron/transfer_app/views.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView, View, DetailView
 from django.db.models import Sum
 from django.shortcuts import render
 
-from orgs.models import Archives
+from orgs.models import Archives, Organization
 from rights.models import RightsStatement
 
 from orgs.authmixins import LoggedInMixinDefaults, OrgReadViewMixin, ArchivistMixin
@@ -18,12 +18,17 @@ class MainView(LoggedInMixinDefaults, TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MainView, self).get_context_data(**kwargs)
 
+        if self.request.user.is_archivist():
+            organization = Organization.objects.all()
+        else:
+            organization = Organization.objects.filter(id=self.request.user.organization.pk)
+
         context['meta_page_title'] = "Dashboard"
-        context['uploads'] = Archives.objects.filter(process_status__gte=20, organization = self.request.user.organization).order_by('-created_time')[:15]
-        context['uploads_count'] = Archives.objects.filter(process_status__gte=20, organization = self.request.user.organization).count()
-        context['validated_count'] = Archives.objects.filter(process_status__gte=40, organization = self.request.user.organization).count()
-        context['accepted_count'] = Archives.objects.filter(process_status__gte=70, organization = self.request.user.organization).count()
-        context['accessioned_count'] = Archives.objects.filter(process_status__gte=90, organization = self.request.user.organization).count()
+        context['uploads'] = Archives.objects.filter(process_status__gte=20, organization__in=organization).order_by('-created_time')[:15]
+        context['uploads_count'] = Archives.objects.filter(process_status__gte=20, organization__in=organization).count()
+        context['validated_count'] = Archives.objects.filter(process_status__gte=40, organization__in=organization).count()
+        context['accepted_count'] = Archives.objects.filter(process_status__gte=70, organization__in=organization).count()
+        context['accessioned_count'] = Archives.objects.filter(process_status__gte=90, organization__in=organization).count()
         context['month_labels'] = []
         context['upload_count_by_month'] = []
         context['upload_size_by_month'] = []
@@ -31,21 +36,20 @@ class MainView(LoggedInMixinDefaults, TemplateView):
 
         today = datetime.date.today()
         current = today - relativedelta(years=1)
-        print current.month
 
         while current <= today:
             context['month_labels'].append(current.strftime("%B"))
-            upload_count = Archives.objects.filter(process_status__gte=20, organization=self.request.user.organization, machine_file_upload_time__year=current.year, machine_file_upload_time__month=current.month).count()
+            upload_count = Archives.objects.filter(process_status__gte=20, organization__in=organization, machine_file_upload_time__year=current.year, machine_file_upload_time__month=current.month).count()
             context['upload_count_by_month'].append(upload_count)
-            upload_size = Archives.objects.filter(process_status__gte=20, organization = self.request.user.organization, machine_file_upload_time__year=current.year, machine_file_upload_time__month=current.month).aggregate(Sum('machine_file_size'))
+            upload_size = Archives.objects.filter(process_status__gte=20, organization__in=organization, machine_file_upload_time__year=current.year, machine_file_upload_time__month=current.month).aggregate(Sum('machine_file_size'))
             if upload_size['machine_file_size__sum']:
                 context['upload_size_by_month'].append(upload_size['machine_file_size__sum']/1000000)
             else:
                 context['upload_size_by_month'].append(0)
             current += relativedelta(months=1)
 
-        context['upload_count_by_year'] = Archives.objects.filter(process_status__gte=20, organization=self.request.user.organization, machine_file_upload_time__year=current.year).count()
-        year_upload_size = Archives.objects.filter(process_status__gte=20, organization = self.request.user.organization, machine_file_upload_time__year=current.year).aggregate(Sum('machine_file_size'))
+        context['upload_count_by_year'] = Archives.objects.filter(process_status__gte=20, organization__in=organization, machine_file_upload_time__year=current.year).count()
+        year_upload_size = Archives.objects.filter(process_status__gte=20, organization__in=organization, machine_file_upload_time__year=current.year).aggregate(Sum('machine_file_size'))
         if year_upload_size['machine_file_size__sum']:
             context['upload_size_by_year'] = round(year_upload_size['machine_file_size__sum']/1000000, 2)
         else:
@@ -60,18 +64,22 @@ class RecentTransfersView(LoggedInMixinDefaults, View):
     template_name = 'orgs/recent_transfers.html'
 
     def get(self, request, *args, **kwargs):
-        org_archives = Archives.objects.filter(process_status__gte=20, organization = request.user.organization).order_by('-created_time')[:25]
+        if self.request.user.is_archivist():
+            organization = Organization.objects.all()
+        else:
+            organization = Organization.objects.filter(id=self.request.user.organization.pk)
+        org_archives = Archives.objects.filter(process_status__gte=20, organization__in=organization).order_by('-created_time')[:25]
         for archive in org_archives:
             archive.bag_info_data = archive.get_bag_data()
-        user_archives = Archives.objects.filter(process_status__gte=20, organization = request.user.organization, user_uploaded=request.user).order_by('-created_time')[:25]
+        user_archives = Archives.objects.filter(process_status__gte=20, organization__in=organization, user_uploaded=request.user).order_by('-created_time')[:25]
         for archive in user_archives:
             archive.bag_info_data = archive.get_bag_data()
         return render(request, self.template_name, {
-            'meta_page_title' : 'Recent Transfers',
-            'org_uploads' : org_archives,
-            'org_uploads_count' : Archives.objects.filter(process_status__gte=20, organization = request.user.organization).count(),
-            'user_uploads' : user_archives,
-            'user_uploads_count' : Archives.objects.filter(process_status__gte=20, organization = request.user.organization, user_uploaded = request.user).count(),
+            'meta_page_title': 'Recent Transfers',
+            'org_uploads': org_archives,
+            'org_uploads_count': Archives.objects.filter(process_status__gte=20, organization__in=organization).count(),
+            'user_uploads': user_archives,
+            'user_uploads_count': Archives.objects.filter(process_status__gte=20, organization__in=organization, user_uploaded=request.user).count(),
         })
 
 class TransferDetailView(OrgReadViewMixin, DetailView):
