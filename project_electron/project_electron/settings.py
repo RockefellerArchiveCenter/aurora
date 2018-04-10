574bc1b38fc58740aa57204afd2077157ec1d904
Hillel Arnold <helrond@hotmail.com>
2018-04-10 13:37:53 -0400

    incorporating browseable api into site layout

--- project_electron/project_electron/settings.py
+++ project_electron/project_electron/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'project_electron.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'orgs', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
