b1ec4eb0aeb90671352770ad7796f34f1b6aabfe
Hillel Arnold <helrond@hotmail.com>
2018-06-11 10:17:54 -0400

    adding bagit profiles and rights statements

--- setup_objects.py
+++ setup_objects.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import Group
 
 from bag_transfer.models import Organization, User, BagItProfile, BagItProfileBagInfo, BagItProfileBagInfoValues, ManifestsRequired, AcceptSerialization, AcceptBagItVersion, TagManifestsRequired, TagFilesRequired
-from bag_transfer.rights.models import RightsStatement, RightsStatementCopyright, RightsStatementOther, RightsStatementRightsGranted
+from bag_transfer.rights.models import RightsStatement, RightsStatementCopyright, RightsStatementOther, RightsStatementRightsGranted, RecordType
 from bag_transfer.lib.RAC_CMD import add_org
 from aurora import settings
 
@@ -47,10 +47,120 @@ print "Creating organizations"
 if len(orgs) == 0:
     Organization.objects.create(name="Test Organization")
     orgs = Organization.objects.all()
+
     print "Creating BagIt Profile for {}".format(orgs[0])
+    profile = BagItProfile.objects.create(
+        applies_to_organization=orgs[0],
+        source_organization=orgs[0],
+        external_descripton="Test BagIt Profile",
+        contact_email="archive@example.org",
+    )
+    AcceptSerialization.objects.create(
+        name='application/zip',
+        bagit_profile=profile
+    )
+    AcceptSerialization.objects.create(
+        name='application/x-tar',
+        bagit_profile=profile
+    )
+    AcceptSerialization.objects.create(
+        name='application/x-gzip',
+        bagit_profile=profile
+    )
+    AcceptBagItVersion.objects.create(
+        name='0.97',
+        bagit_profile=profile
+    )
+    for field in ['external_identifier', 'internal_sender_description', 'title',
+                  'date_start', 'date_end', 'bagging_date', 'payload_oxum',
+                  'source_organization', 'record_type']:
+        BagItProfileBagInfo.objects.create(
+            bagit_profile=profile,
+            field=field,
+            required=True,
+            repeatable=False
+        )
+    for field in ['record_creators', 'language']:
+        BagItProfileBagInfo.objects.create(
+            bagit_profile=profile,
+            field=field,
+            required=True,
+            repeatable=True
+        )
+    for field in ['bag_count', 'bag_group_identifier']:
+        BagItProfileBagInfo.objects.create(
+            bagit_profile=profile,
+            field=field,
+            required=False,
+            repeatable=False
+        )
+    source_organization = BagItProfileBagInfo.objects.get(field='source_organization')
+    for name in ['Ford Foundation', 'Rockefeller Foundation',
+                 'Rockefeller Brothers Fund', 'Commonwealth Fund']:
+        BagItProfileBagInfoValues.objects.create(
+            bagit_profile_baginfo=source_organization,
+            name=name
+        )
+    record_type = BagItProfileBagInfo.objects.get(field='record_type')
+    for name in ['administrative records', 'board materials', 'grant records',
+                 'communications and publications', 'annual reports']:
+        BagItProfileBagInfoValues.objects.create(
+            bagit_profile_baginfo=record_type,
+            name=name
+        )
+
+    for type in ['administrative records', 'board materials', 'grant records',
+                 'communications and publications', 'annual reports']:
+        RecordType.objects.get_or_create(name=type)
+
     print "Creating Rights Statements for {}".format(orgs[0])
-    # Copyright
-    # Other/Donor
+    copyright_statement = RightsStatement.objects.create(
+        organization=orgs[0],
+        rights_basis="Copyright",
+    )
+    for record_type in RecordType.objects.all():
+        copyright_statement.applies_to_type.add(record_type)
+
+    donor_statement = RightsStatement.objects.create(
+        organization=orgs[0],
+        rights_basis="Other",
+    )
+    for record_type in RecordType.objects.all():
+        donor_statement.applies_to_type.add(record_type)
+
+    RightsStatementCopyright.objects.create(
+        rights_statement=copyright_statement,
+        copyright_status='copyrighted',
+        copyright_end_date_period=40,
+        copyright_note='Work for hire, under copyright until 40 years after creation.',
+    )
+    RightsStatementOther.objects.create(
+        rights_statement=donor_statement,
+        other_rights_basis='Donor',
+        other_rights_end_date_period=10,
+        other_rights_note='Records embargoed for 10 years after date of creation.',
+    )
+    RightsStatementRightsGranted.objects.create(
+        rights_statement=copyright_statement,
+        act='publish',
+        end_date_period=40,
+        rights_granted_note='Records under copyright may not be republished without permission from the copyright holder.',
+        restriction='disallow',
+    )
+    RightsStatementRightsGranted.objects.create(
+        rights_statement=donor_statement,
+        act='disseminate',
+        start_date_period=10,
+        rights_granted_note='Records are open for research after expiration of embargo period.',
+        restriction='allow',
+    )
+    RightsStatementRightsGranted.objects.create(
+        rights_statement=donor_statement,
+        act='disseminate',
+        end_date_period=10,
+        rights_granted_note='Records are open for research after expiration of embargo period.',
+        restriction='disallow',
+    )
 else:
     for org in orgs:
         org_ids.append(org.machine_name[3:])
