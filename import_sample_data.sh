e3c34a1c2eead05c39682dde41f466a79bff2797
Hillel Arnold <helrond@hotmail.com>
2019-01-21 20:33:07 -0500

    updating sample bags for new object structure

--- import_sample_data.sh
+++ import_sample_data.sh
@@ -3,6 +3,6 @@
 # Script to import set of sample bags
 # Copies files to data upload directory and then runs cron
 
-cp -r ../sample_bags/* /data/org1/upload/
-chown -R admin /data/org1/upload/
+cp -r ../sample_bags/* /data/org2/upload/
+chown -R donor /data/org2/upload/
 python manage.py runcrons bag_transfer.lib.cron.DiscoverTransfers
--- setup_objects.py
+++ setup_objects.py
@@ -8,6 +8,16 @@ from aurora import settings
 
 orgs = Organization.objects.all()
 org_ids = []
+DEFAULT_ORGS = [
+    {
+        "name": "Archival Repository",
+        "acquisition_type": "donation"
+    },
+    {
+        "name": "Donor Organization",
+        "acquisition_type": "deposit"
+    }
+]
 DEFAULT_USERS = [
     {
         "username": "admin",
@@ -16,6 +26,7 @@ DEFAULT_USERS = [
         "last_name": "Administrator",
         "superuser": True,
         "staff": True,
+        "org": "Archival Repository"
     },
     {
         "username": "donor",
@@ -24,6 +35,7 @@ DEFAULT_USERS = [
         "last_name": "Representative",
         "superuser": False,
         "staff": False,
+        "org": "Donor Organization"
     },
     {
         "username": "manager",
@@ -33,6 +45,7 @@ DEFAULT_USERS = [
         "superuser": False,
         "staff": True,
         "groups": ["managing_archivists"],
+        "org": "Archival Repository"
     },
     {
         "username": "appraiser",
@@ -42,6 +55,7 @@ DEFAULT_USERS = [
         "superuser": False,
         "staff": True,
         "groups": ["appraisal_archivists"],
+        "org": "Archival Repository"
     },
     {
         "username": "accessioner",
@@ -51,125 +65,128 @@ DEFAULT_USERS = [
         "superuser": False,
         "staff": True,
         "groups": ["accessioning_archivists"],
+        "org": "Archival Repository"
     }
 ]
 
+print "Creating record types"
+for type in ['administrative records', 'board materials', 'grant records',
+             'communications and publications', 'annual reports']:
+    RecordType.objects.get_or_create(name=type)
+
 print "Creating organizations"
 if len(orgs) == 0:
-    Organization.objects.create(name="Test Organization", acquisition_type="donation")
-    orgs = Organization.objects.all()
+    for org in DEFAULT_ORGS:
+        new_org = Organization.objects.create(name=org['name'], acquisition_type=org['acquisition_type'])
+        archive_org = Organization.objects.get(name="Archival Repository")
 
-    print "Creating BagIt Profile for {}".format(orgs[0])
-    profile = BagItProfile.objects.create(
-        applies_to_organization=orgs[0],
-        source_organization=orgs[0],
-        external_description="Test BagIt Profile",
-        contact_email="archive@example.org",
-    )
-    AcceptSerialization.objects.create(
-        name='application/zip',
-        bagit_profile=profile
-    )
-    AcceptSerialization.objects.create(
-        name='application/x-tar',
-        bagit_profile=profile
-    )
-    AcceptSerialization.objects.create(
-        name='application/x-gzip',
-        bagit_profile=profile
-    )
-    AcceptBagItVersion.objects.create(
-        name='0.97',
-        bagit_profile=profile
-    )
-    for field in ['external_identifier', 'internal_sender_description', 'title',
-                  'date_start', 'date_end', 'bagging_date', 'payload_oxum',
-                  'source_organization', 'record_type']:
-        BagItProfileBagInfo.objects.create(
-            bagit_profile=profile,
-            field=field,
-            required=True,
-            repeatable=False
+        print "Creating BagIt Profile for {}".format(new_org)
+        profile = BagItProfile.objects.create(
+            applies_to_organization=new_org,
+            source_organization=archive_org,
+            external_description="Test BagIt Profile",
+            contact_email="archive@example.org",
         )
-    for field in ['record_creators', 'language']:
-        BagItProfileBagInfo.objects.create(
-            bagit_profile=profile,
-            field=field,
-            required=True,
-            repeatable=True
+        AcceptSerialization.objects.create(
+            name='application/zip',
+            bagit_profile=profile
         )
-    for field in ['bag_count', 'bag_group_identifier']:
-        BagItProfileBagInfo.objects.create(
-            bagit_profile=profile,
-            field=field,
-            required=False,
-            repeatable=False
+        AcceptSerialization.objects.create(
+            name='application/x-tar',
+            bagit_profile=profile
         )
-    source_organization = BagItProfileBagInfo.objects.get(field='source_organization')
-    BagItProfileBagInfoValues.objects.create(
-        bagit_profile_baginfo=source_organization,
-        name='Test Organization'
-    )
-    record_type = BagItProfileBagInfo.objects.get(field='record_type')
-    for name in ['administrative records', 'board materials', 'grant records',
-                 'communications and publications', 'annual reports']:
+        AcceptSerialization.objects.create(
+            name='application/x-gzip',
+            bagit_profile=profile
+        )
+        AcceptBagItVersion.objects.create(
+            name='0.97',
+            bagit_profile=profile
+        )
+        for field in ['external_identifier', 'internal_sender_description', 'title',
+                      'date_start', 'date_end', 'bagging_date', 'payload_oxum',
+                      'source_organization', 'record_type']:
+            BagItProfileBagInfo.objects.create(
+                bagit_profile=profile,
+                field=field,
+                required=True,
+                repeatable=False
+            )
+        for field in ['record_creators', 'language']:
+            BagItProfileBagInfo.objects.create(
+                bagit_profile=profile,
+                field=field,
+                required=True,
+                repeatable=True
+            )
+        for field in ['bag_count', 'bag_group_identifier']:
+            BagItProfileBagInfo.objects.create(
+                bagit_profile=profile,
+                field=field,
+                required=False,
+                repeatable=False
+            )
+        source_organization = BagItProfileBagInfo.objects.get(field='source_organization', bagit_profile=profile)
         BagItProfileBagInfoValues.objects.create(
-            bagit_profile_baginfo=record_type,
-            name=name
+            bagit_profile_baginfo=source_organization,
+            name=new_org.name
         )
+        record_type = BagItProfileBagInfo.objects.get(field='record_type', bagit_profile=profile)
+        for name in ['administrative records', 'board materials', 'grant records',
+                     'communications and publications', 'annual reports']:
+            BagItProfileBagInfoValues.objects.create(
+                bagit_profile_baginfo=record_type,
+                name=name
+            )
 
-    for type in ['administrative records', 'board materials', 'grant records',
-                 'communications and publications', 'annual reports']:
-        RecordType.objects.get_or_create(name=type)
+        print "Creating Rights Statements for {}".format(new_org)
+        copyright_statement = RightsStatement.objects.create(
+            organization=new_org,
+            rights_basis="Copyright",
+        )
+        for record_type in RecordType.objects.all():
+            copyright_statement.applies_to_type.add(record_type)
 
-    print "Creating Rights Statements for {}".format(orgs[0])
-    copyright_statement = RightsStatement.objects.create(
-        organization=orgs[0],
-        rights_basis="Copyright",
-    )
-    for record_type in RecordType.objects.all():
-        copyright_statement.applies_to_type.add(record_type)
+        donor_statement = RightsStatement.objects.create(
+            organization=new_org,
+            rights_basis="Other",
+        )
+        for record_type in RecordType.objects.all():
+            donor_statement.applies_to_type.add(record_type)
 
-    donor_statement = RightsStatement.objects.create(
-        organization=orgs[0],
-        rights_basis="Other",
-    )
-    for record_type in RecordType.objects.all():
-        donor_statement.applies_to_type.add(record_type)
-
-    RightsStatementCopyright.objects.create(
-        rights_statement=copyright_statement,
-        copyright_status='copyrighted',
-        copyright_end_date_period=40,
-        copyright_note='Work for hire, under copyright until 40 years after creation.',
-    )
-    RightsStatementOther.objects.create(
-        rights_statement=donor_statement,
-        other_rights_basis='Donor',
-        other_rights_end_date_period=10,
-        other_rights_note='Records embargoed for 10 years after date of creation.',
-    )
-    RightsStatementRightsGranted.objects.create(
-        rights_statement=copyright_statement,
-        act='publish',
-        end_date_period=40,
-        rights_granted_note='Records under copyright may not be republished without permission from the copyright holder.',
-        restriction='disallow',
-    )
-    RightsStatementRightsGranted.objects.create(
-        rights_statement=donor_statement,
-        act='disseminate',
-        start_date_period=10,
-        rights_granted_note='Records are open for research after expiration of embargo period.',
-        restriction='allow',
-    )
-    RightsStatementRightsGranted.objects.create(
-        rights_statement=donor_statement,
-        act='disseminate',
-        end_date_period=10,
-        rights_granted_note='Records are open for research after expiration of embargo period.',
-        restriction='disallow',
-    )
+        RightsStatementCopyright.objects.create(
+            rights_statement=copyright_statement,
+            copyright_status='copyrighted',
+            copyright_end_date_period=40,
+            copyright_note='Work for hire, under copyright until 40 years after creation.',
+        )
+        RightsStatementOther.objects.create(
+            rights_statement=donor_statement,
+            other_rights_basis='Donor',
+            other_rights_end_date_period=10,
+            other_rights_note='Records embargoed for 10 years after date of creation.',
+        )
+        RightsStatementRightsGranted.objects.create(
+            rights_statement=copyright_statement,
+            act='publish',
+            end_date_period=40,
+            rights_granted_note='Records under copyright may not be republished without permission from the copyright holder.',
+            restriction='disallow',
+        )
+        RightsStatementRightsGranted.objects.create(
+            rights_statement=donor_statement,
+            act='disseminate',
+            start_date_period=10,
+            rights_granted_note='Records are open for research after expiration of embargo period.',
+            restriction='allow',
+        )
+        RightsStatementRightsGranted.objects.create(
+            rights_statement=donor_statement,
+            act='disseminate',
+            end_date_period=10,
+            rights_granted_note='Records are open for research after expiration of embargo period.',
+            restriction='disallow',
+        )
 else:
     for org in orgs:
         org_ids.append(org.machine_name[3:])
@@ -196,9 +213,12 @@ if len(User.objects.all()) == 0:
             email="{}@example.org".format(user['username']),
             is_superuser=user['superuser'],
             is_staff=user['staff'],
-            organization=orgs[0])
+            organization=Organization.objects.get(name=user['org']))
         if 'groups' in user:
             for group in user['groups']:
                 g = Group.objects.get_or_create(name=group)[0]
                 new_user.groups.add(g)
         new_user.save()
+else:
+    for user in User.objects.all():
+        user.save()
