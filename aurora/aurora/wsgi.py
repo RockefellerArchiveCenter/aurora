21fe9a75181a6604d0024145a5d184ebb43915c0
Hillel Arnold <helrond@hotmail.com>
2020-04-30 16:30:24 -0500

    better handling for python path

--- aurora/aurora/wsgi.py
+++ aurora/aurora/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_electron.settings")
+path = '/data/htdocs/aurora/aurora'
+
+try:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bag_transfer.settings")
+except ModuleNotFoundError:
+    if path not in sys.path:
+        sys.path.append(path)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bag_transfer.settings")
 
 application = get_wsgi_application()
