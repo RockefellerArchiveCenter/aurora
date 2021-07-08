e741d00f00ddc6d8d4603240708520bc751fc550
Hillel Arnold <helrond@hotmail.com>
2021-07-07 21:31:19 -0400

    add tests for signals

---
+++ aurora/bag_transfer/test/test_signals.py
@@ -0,0 +1,21 @@
+import random
+
+from bag_transfer.accession.models import Accession
+from bag_transfer.models import Archives
+from django.test import TestCase
+
+
+class SignalsTestCase(TestCase):
+    fixtures = ["complete.json"]
+
+    def test_update_accession_status(self):
+        """
+        Ensures that Accession process status is updated when process status for
+        all associated archives is updated.
+        """
+        accession = random.choice(Accession.objects.all())
+        for archive in accession.accession_transfers.all():
+            archive.process_status = Archives.ACCESSIONING_COMPLETE
+            archive.save()
+        accession.refresh_from_db()
+        self.assertEqual(accession.process_status, Accession.COMPLETE)
