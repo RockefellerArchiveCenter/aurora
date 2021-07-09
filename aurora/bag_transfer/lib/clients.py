3919d325d05b496897e18a7a15b435d851fa5ca1
Hillel Arnold <helrond@hotmail.com>
2021-07-09 13:53:39 -0400

    use archivessnake in as client

--- aurora/bag_transfer/lib/clients.py
+++ aurora/bag_transfer/lib/clients.py
@@ -1,6 +1,4 @@
-import json
-
-from requests import Session
+from asnake.client import ASnakeClient
 
 
 class ArchivesSpaceClientError(Exception):
@@ -9,42 +7,16 @@ class ArchivesSpaceClientError(Exception):
 
 class ArchivesSpaceClient:
     def __init__(self, baseurl, username, password, repo_id):
+        self.client = ASnakeClient(
+            baseurl=baseurl,
+            username=username,
+            password=password)
         self.repo_id = repo_id
-        self.baseurl = baseurl
-        self.session = Session()
-        self.session.headers.update({"Accept": "application/json"})
-        try:
-            self.authorize(username, password)
-        except Exception as e:
-            raise ArchivesSpaceClientError(
-                "Couldn't authenticate user credentials for ArchivesSpace: {}".format(e)
-            )
 
-    def get_resource(self, resource_id, *args, **kwargs):
-        resp = self.session.get(
-            "/".join(
-                [
-                    self.baseurl.rstrip("/"),
-                    "/repositories/{repo_id}/resources/{resource_id}",
-                ]
-            ).format(repo_id=self.repo_id, resource_id=resource_id)
-        )
-        if resp.status_code == 200:
-            return resp.json()
+    def get_resource(self, resource_id):
+        """Returns a JSON representation of a resource, or raises an exception if an error occurs."""
+        resource = self.client.get("/repositories/{}/resources/{}".format(self.repo_id, resource_id))
+        if resource.status_code == 200:
+            return resource.json()
         else:
-            raise ArchivesSpaceClientError(resp.json()["error"])
-
-    def authorize(self, username, password):
-        resp = self.session.post(
-            "/".join([self.baseurl.rstrip("/"), "users/{username}/login"]).format(
-                username=username
-            ),
-            params={"password": password, "expiring": False},
-        )
-
-        if resp.status_code != 200:
-            raise ArchivesSpaceClientError((resp.status_code))
-        else:
-            session_token = json.loads(resp.text)["session"]
-            self.session.headers["X-ArchivesSpace-Session"] = session_token
-            return session_token
+            raise ArchivesSpaceClientError(resource.json()["error"])
