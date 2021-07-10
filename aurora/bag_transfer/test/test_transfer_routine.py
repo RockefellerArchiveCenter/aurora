97042b4cf6d9b37a77d6d777c58671186ffd210e
Hillel Arnold <helrond@hotmail.com>
2021-07-10 18:04:58 -0400

    enhance transfer routine tests

--- aurora/bag_transfer/test/test_transfer_routine.py
+++ aurora/bag_transfer/test/test_transfer_routine.py
@@ -4,9 +4,11 @@ import shutil
 
 from asterism.file_helpers import remove_file_or_dir
 from aurora import settings
+from bag_transfer.lib.bag_checker import BagChecker
 from bag_transfer.lib.transfer_routine import TransferRoutine
-from bag_transfer.models import Archives, DashboardMonthData, Organization
-from bag_transfer.test import helpers
+from bag_transfer.models import (Archives, DashboardMonthData, Organization,
+                                 User)
+from bag_transfer.test import helpers, setup
 from django.test import TestCase
 
 
@@ -19,65 +21,67 @@ class TransferRoutineTestCase(TestCase):
         self.reset_org_dirs(Organization.objects.all())
 
     def test_setup_routine(self):
+        """Asserts TransferRoutine sets up correctly."""
         self.routine = TransferRoutine()
         self.sub_test_db_has_active_orgs()
         self.sub_test_verify_organizations_paths()
         self.sub_test_no_orgs_with_dirs()
         self.sub_test_contents_dictionary()
 
-    # def test_run_routine(self):
-    #
-    #     test_on_BagChecker = [r[0] for r in setup.BAGS_REF if len(r) > 2 and r[2]]
-    #     test_on_transfer_routine = [r[0] for r in setup.BAGS_REF if len(r) > 3 and r[3]]
-    #
-    #     for ref in setup.BAGS_REF:
-    #
-    #         helpers.create_target_bags(ref[0], settings.TEST_BAGS_DIR, self.orgs[0])
-    #         tr = TransferRoutine()
-    #         self.assertTrue(tr.setup_routine(), "Transfer routine did not set up properly")
-    #         self.assertTrue(isinstance(tr.run_routine(), dict), "Expected transfer routine to produce a dict, got {} instead".format(tr.run_routine()))
-    #
-    #         for trans in tr.transfers:
-    #             if not trans["file_name"].startswith(ref[0]):
-    #                 continue
-    #             if ref[0] == "valid_bag":
-    #                 self.assertFalse(trans["auto_fail"])
-    #             else:
-    #                 if ref[0] in test_on_transfer_routine:
-    #                     self.assertTrue(trans["auto_fail"])
-    #                     self.assertEqual(ref[1], trans["auto_fail_code"])
-    #
-    #             archive = Archives.initial_save(
-    #                 self.orgs[0],
-    #                 None,
-    #                 trans["file_path"],
-    #                 trans["file_size"],
-    #                 trans["file_modtime"],
-    #                 Archives().gen_identifier(),
-    #                 trans["file_type"],
-    #                 trans["bag_it_name"])
-    #             archive.organization.name = "Ford Foundation"
-    #             archive.organization.save()
-    #
-    #             self.assertIsNot(False, archive.machine_file_identifier, "Expected bag identifier to exist")
-    #
-    #             if trans["auto_fail"]:
-    #                 continue
-    #
-    #             bag = BagChecker(archive)
-    #             passed_all_results = bag.bag_passed_all()
-    #
-    #             if ref[0] in ["valid_bag", "no_metadata_file"]:
-    #                 self.assertTrue(passed_all_results, "Bag unexpectedly invalid")
-    #             else:
-    #                 self.assertFalse(passed_all_results, "Bag unexpectedly valid")
-    #                 if ref[0] in test_on_BagChecker:
-    #                     self.assertEqual(ref[1], bag.ecode)
-    #
-    #             # deleting path in processing and tmp dir
-    #             remove_file_or_dir(
-    #                 os.path.join(settings.TRANSFER_EXTRACT_TMP, archive.bag_it_name))
-    #             remove_file_or_dir(archive.machine_file_path)
+    def test_run_routine(self):
+        """Asserts TransferRoutine handles valid and invalid bags."""
+        test_on_BagChecker = [r[0] for r in setup.BAGS_REF if len(r) > 2 and r[2]]
+        test_on_transfer_routine = [r[0] for r in setup.BAGS_REF if len(r) > 3 and r[3]]
+        org = random.choice(Organization.objects.all())
+        user = random.choice(User.objects.filter(organization=org))
+
+        for prefix, error, *rest in setup.BAGS_REF:
+
+            helpers.create_target_bags(
+                prefix, settings.TEST_BAGS_DIR, org, user.username)
+            tr = TransferRoutine()
+            routine = tr.run_routine()
+            self.assertTrue(
+                isinstance(routine, list),
+                "Expected transfer routine to produce a list, got {} instead".format(routine))
+
+            for trans in tr.transfers:
+                if not trans["file_name"].startswith(prefix):
+                    continue
+                if prefix == "valid_bag":
+                    self.assertFalse(trans["auto_fail"])
+                else:
+                    if prefix in test_on_transfer_routine:
+                        self.assertTrue(trans["auto_fail"])
+                        self.assertEqual(error, trans["auto_fail_code"])
+
+                archive = Archives.objects.create(
+                    organization=org,
+                    user_uploaded=user,
+                    machine_file_path=trans["file_path"],
+                    machine_file_size=trans["file_size"],
+                    machine_file_upload_time=trans["file_modtime"],
+                    machine_file_identifier=Archives().gen_identifier(),
+                    machine_file_type=trans["file_type"],
+                    bag_it_name=trans["bag_it_name"])
+
+                if trans["auto_fail"]:
+                    continue
+
+                bag = BagChecker(archive)
+                passed_all_results = bag.bag_passed_all()
+
+                if prefix in ["valid_bag", "no_metadata_file"]:
+                    self.assertTrue(passed_all_results, "Bag unexpectedly invalid")
+                else:
+                    self.assertFalse(passed_all_results, "Bag unexpectedly valid")
+                    if prefix in test_on_BagChecker:
+                        self.assertEqual(error, bag.ecode)
+
+                # deleting path in processing and tmp dir
+                remove_file_or_dir(
+                    os.path.join(settings.TRANSFER_EXTRACT_TMP, archive.bag_it_name))
+                remove_file_or_dir(archive.machine_file_path)
 
     def sub_test_db_has_active_orgs(self):
         """Asserts TransferRoutine setup handles inactive organizations."""
@@ -102,7 +106,7 @@ class TransferRoutineTestCase(TestCase):
         self.assertEqual(
             original_active_count - 1, len(self.routine.active_organizations),
             "Expected one organization to have been removed from TransferRoutine.active_organizations")
-        self.reset_org_dirs(last_org)
+        self.reset_org_dirs([last_org])
 
     def sub_test_no_orgs_with_dirs(self):
         self.change_all_orgs_in_list_status(Organization.objects.all(), True)
@@ -117,10 +121,18 @@ class TransferRoutineTestCase(TestCase):
 
     def sub_test_contents_dictionary(self):
         """Asserts the TransferRoutine contents dictionary is correctly set up."""
-        self.assertFalse(self.routine.setup_routine(), "Expected TransferRoutine setup to fail because there are no transfers in the transfer directory")
+        self.assertFalse(
+            self.routine.setup_routine(),
+            "Expected TransferRoutine setup to fail because there are no transfers in the transfer directory")
+        self.assertFalse(
+            self.routine.has_setup_err,
+            "Expected TransferRoutine.has_setup_err to be False because there are no transfers in the transfer directory")
         organization = random.choice(Organization.objects.filter(is_active=True))
         helpers.create_target_bags("valid_bag", settings.TEST_BAGS_DIR, organization)
         self.assertTrue(self.routine.setup_routine(), "Expected TransferRoutine setup to succeed.")
+        self.assertTrue(
+            isinstance(self.routine.routine_contents_dictionary, dict),
+            "Expecting TransferRoutine.routine_contents_dictionary to be a dict")
         self.reset_org_dirs(Organization.objects.all())
 
     def change_all_orgs_in_list_status(self, orgs={}, Status=False):
