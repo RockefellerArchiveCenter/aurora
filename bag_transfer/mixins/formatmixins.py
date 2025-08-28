9136d224a63314fc34f0fd8d79f2b38ae07a4ef2
Hillel Arnold <hillel.arnold@gmail.com>
2025-08-28 10:34:24 -0700

    allow CSV mixin to be used for accessions

--- bag_transfer/mixins/formatmixins.py
+++ bag_transfer/mixins/formatmixins.py
@@ -5,10 +5,9 @@ from django.http import HttpResponse, JsonResponse
 
 
 class CSVResponseMixin(object):
-    csv_filename = "transfers-{}.csv".format(datetime.datetime.now())
 
     def get_csv_filename(self):
-        return self.csv_filename
+        return f"{self.prefix}-{datetime.datetime.now()}.csv"
 
     def render_to_csv(self, data):
         response = HttpResponse(content_type="text/csv")
