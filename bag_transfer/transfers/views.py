4f8edf9cbca6b6d063e03913b7b298e0915cc4d5
Hillel Arnold <hillel.arnold@gmail.com>
2025-08-27 17:20:06 -0700

    only pull queryset once

--- bag_transfer/transfers/views.py
+++ bag_transfer/transfers/views.py
@@ -27,19 +27,24 @@ class DashboardView(PageTitleMixin, LoggedInMixinDefaults, TemplateView):
             users (Queryset): list of users associated with the organizations
         """
         org_uploads = Transfer.objects.filter(process_status__gte=Transfer.TRANSFER_COMPLETED, organization__in=orgs)
+        validated_uploads = org_uploads.filter(process_status__gte=Transfer.VALIDATED)
+        accepted_uploads = org_uploads.filter(process_status__gte=Transfer.ACCEPTED)
+        accessioned_uploads = org_uploads.filter(process_status__gte=Transfer.ACCESSIONING_COMPLETE)
+
         data = {
             "name": org_name,
             "users": users,
             "uploads": org_uploads.order_by("-created_time")[:10],
             "uploads_count": org_uploads.count(),
-            "validated_count": org_uploads.filter(process_status__gte=Transfer.VALIDATED).count(),
-            "accepted_count": org_uploads.filter(process_status__gte=Transfer.ACCEPTED).count(),
-            "accessioned_count": org_uploads.filter(process_status__gte=Transfer.ACCESSIONING_COMPLETE).count(),
+            "validated_count": validated_uploads.count(),
+            "accepted_count": accepted_uploads.count(),
+            "accessioned_count": accessioned_uploads.count(),
             "uploads_size": self.get_upload_size(org_uploads),
-            "validated_size": self.get_upload_size(org_uploads.filter(process_status__gte=Transfer.VALIDATED)),
-            "accepted_size": self.get_upload_size(org_uploads.filter(process_status__gte=Transfer.ACCEPTED)),
-            "accessioned_size": self.get_upload_size(org_uploads.filter(process_status__gte=Transfer.ACCESSIONING_COMPLETE)),
+            "validated_size": self.get_upload_size(validated_uploads),
+            "accepted_size": self.get_upload_size(accepted_uploads),
+            "accessioned_size": self.get_upload_size(accessioned_uploads),
         }
+
         return data
 
     def get_context_data(self, **kwargs):
